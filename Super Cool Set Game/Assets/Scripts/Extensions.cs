9908d31aec608a7da892a3dca4010cb34e31ef00
Alex Habich <me@alexnader.dev>
2020-10-28 10:28:13 -0500

    Remove unused extension

--- Super Cool Set Game/Assets/Scripts/Extensions.cs
+++ Super Cool Set Game/Assets/Scripts/Extensions.cs
@@ -14,8 +14,4 @@ public static class Extensions {
             yield return self.GetChild(i);
         }
     }
-
-    private static Vector2 XY(this Vector3 v) {
-        return new Vector2(v.x, v.y);
-    }
 }
